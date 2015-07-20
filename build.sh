87e0f1b624908a32f56393cea2e554733f8506b9
Julien Viet <julien@julienviet.com>
2015-07-20 11:54:53 +0200

    Updated to download eclipse

--- build.sh
+++ build.sh
@@ -1,7 +1,23 @@
-ceylon_path=`pwd`/ceylon-dist/dist/bin
+CEYLON_PATH=`pwd`/ceylon-dist/dist/bin
 
-(cd ceylon-dist;ant setup clean publish-all ide-quick)
-(cd ceylon-sdk;ant clean publish ide-quick -Ddist.bin.dir=$ceylon_path)
-(cd ceylon.formatter;ant clean publish ide-quick -Ddist.bin.dir=$ceylon_path)
-(cd ceylon-ide-common;ant clean publish ide-quick -Ddist.bin.dir=$ceylon_path)
-(cd ceylon-ide-eclipse;mvn clean install -DskipTests)
+if [ -f eclipse.tgz ];
+then
+    echo "Eclipse already downloaded"
+else
+    echo "Downloading Eclipse"
+    curl -o eclipse.tgz --remote-name http://mirrors.ibiblio.org/eclipse/technology/epp/downloads/release/kepler/SR2/eclipse-standard-kepler-SR2-linux-gtk-x86_64.tar.gz
+fi
+if [ -f eclipse.tgz ];
+then
+    echo "Eclipse already unzipped"
+else
+    echo "Unzipping Eclipse"
+    tar -xvzf eclipse.tgz
+fi
+$ECLIPSE_PATH=`pwd`/eclipse
+PATH=$ECLIPSE_PATH:$PATH
+# (cd ceylon-dist;ant setup clean publish-all ide-quick)
+# (cd ceylon-sdk;ant clean publish ide-quick -Ddist.bin.dir=$CEYLON_PATH)
+# (cd ceylon.formatter;ant clean publish ide-quick -Ddist.bin.dir=$CEYLON_PATH)
+# (cd ceylon-ide-common;ant clean publish ide-quick -Ddist.bin.dir=$CEYLON_PATH)
+# (cd ceylon-ide-eclipse;mvn clean install -DskipTests)
