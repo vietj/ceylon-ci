911d79c321815b73128a37a35cc0bc97cfe18530
Julien Viet <julien@julienviet.com>
2015-07-20 12:03:31 +0200

    Fixes

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ else
     echo "Downloading Eclipse"
     curl -o eclipse.tgz --remote-name http://mirrors.ibiblio.org/eclipse/technology/epp/downloads/release/kepler/SR2/eclipse-standard-kepler-SR2-linux-gtk-x86_64.tar.gz
 fi
-if [ -f eclipse.tgz ];
+if [ -f eclipse ];
 then
     echo "Eclipse already unzipped"
 else
@@ -16,8 +16,8 @@ else
 fi
 $ECLIPSE_PATH=`pwd`/eclipse
 PATH=$ECLIPSE_PATH:$PATH
-# (cd ceylon-dist;ant setup clean publish-all ide-quick)
-# (cd ceylon-sdk;ant clean publish ide-quick -Ddist.bin.dir=$CEYLON_PATH)
-# (cd ceylon.formatter;ant clean publish ide-quick -Ddist.bin.dir=$CEYLON_PATH)
-# (cd ceylon-ide-common;ant clean publish ide-quick -Ddist.bin.dir=$CEYLON_PATH)
-# (cd ceylon-ide-eclipse;mvn clean install -DskipTests)
+(cd ceylon-dist;ant setup clean publish-all ide-quick)
+(cd ceylon-sdk;ant clean publish ide-quick -Ddist.bin.dir=$CEYLON_PATH)
+(cd ceylon.formatter;ant clean publish ide-quick -Ddist.bin.dir=$CEYLON_PATH)
+(cd ceylon-ide-common;ant clean publish ide-quick -Ddist.bin.dir=$CEYLON_PATH)
+(cd ceylon-ide-eclipse;mvn clean install -DskipTests)
