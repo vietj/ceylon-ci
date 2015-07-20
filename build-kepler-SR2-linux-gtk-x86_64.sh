1fda8627ca2b7551c8f039d12314a55992654918
Julien Viet <julien@julienviet.com>
2015-07-20 12:39:53 +0200

    Execute linux build in a separate build

---
+++ build-kepler-SR2-linux-gtk-x86_64.sh
@@ -0,0 +1,17 @@
+if [ -f eclipse.tgz ];
+then
+    echo "Eclipse already downloaded"
+else
+    echo "Downloading Eclipse"
+    curl -o eclipse.tgz --remote-name http://mirrors.ibiblio.org/eclipse/technology/epp/downloads/release/kepler/SR2/eclipse-standard-kepler-SR2-linux-gtk-x86_64.tar.gz
+fi
+if [ -f eclipse ];
+then
+    echo "Eclipse already unzipped"
+else
+    echo "Unzipping Eclipse"
+    tar -xvzf eclipse.tgz
+fi
+ECLIPSE_PATH=`pwd`/eclipse
+PATH=$ECLIPSE_PATH:$PATH
+sh ./build.sh
