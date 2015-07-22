e3b5423bd3ae08c9d1cfd3f72efc6bc040651788
Julien Viet <julien@julienviet.com>
2015-07-22 14:16:06 +0200

    Remove useless kepler / linux build

--- build-kepler-SR2-linux-gtk-x86_64.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -f eclipse.tgz ];
-then
-    curl -s -o eclipse.tgz --remote-name http://mirrors.ibiblio.org/eclipse/technology/epp/downloads/release/kepler/SR2/eclipse-standard-kepler-SR2-linux-gtk-x86_64.tar.gz
-fi
-ECLIPSE_PATH=`pwd`/eclipse
-rm  -rf $ECLIPSE_PATH
-tar -xvzf eclipse.tgz
-PATH=$ECLIPSE_PATH:$PATH
-sh ./build.sh
