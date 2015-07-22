8338fd88d1d29a545c21be609a80b8a9a0000537
Julien Viet <julien@julienviet.com>
2015-07-22 10:23:33 +0200

    Fix linux script

--- build-kepler-SR2-linux-gtk-x86_64.sh
+++ build-kepler-SR2-linux-gtk-x86_64.sh
@@ -1,4 +1,5 @@
 if [ ! -f eclipse.tgz ];
+then
     curl -s -o eclipse.tgz --remote-name http://mirrors.ibiblio.org/eclipse/technology/epp/downloads/release/kepler/SR2/eclipse-standard-kepler-SR2-linux-gtk-x86_64.tar.gz
 fi
 ECLIPSE_PATH=`pwd`/eclipse
