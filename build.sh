1adcc00aa6442c871b56b86bd39cd95b21382926
Julien Viet <julien@julienviet.com>
2015-11-23 08:06:06 +0100

    Update to latest

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 set -e
 
 # Clone repos
-git clone --depth 1 https://github.com/ceylon/ceylon-dist.git
-git clone --depth 1 https://github.com/ceylon/ceylon-ide-common.git
-git clone --depth 1 https://github.com/ceylon/ceylon-ide-eclipse.git
+git clone --depth 1 https://github.com/ceylon/ceylon.git
 git clone --depth 1 https://github.com/ceylon/ceylon-sdk.git
 git clone --depth 1 https://github.com/ceylon/ceylon.formatter.git
+git clone --depth 1 https://github.com/ceylon/ceylon-ide-common.git
 git clone --depth 1 https://github.com/ceylon/ceylon.tool.converter.java2ceylon.git
+git clone --depth 1 https://github.com/ceylon/ceylon-ide-eclipse.git
 
 # Ceylon repo
 CEYLON_REPO=`pwd`/repo
@@ -22,8 +22,8 @@ fi
 printf '%s\n%s\n' '[repository "USER"]' "url=$CEYLON_REPO" >.ceylon/config
 
 # Build
-CEYLON_PATH=`pwd`/ceylon-dist/dist/bin
-(cd ceylon-dist;ant setup clean publish ide-quick)
+CEYLON_PATH=`pwd`/ceylon/dist/bin
+(cd ceylon/dist;ant clean publish ide-quick)
 (cd ceylon-sdk;ant clean publish ide-quick -Ddist.bin.dir=$CEYLON_PATH -Dceylon.repo.dir=$CEYLON_REPO)
 (cd ceylon.formatter;ant clean publish ide-quick -Ddist.bin.dir=$CEYLON_PATH -Dceylon.repo.dir=$CEYLON_REPO)
 (cd ceylon-ide-common;ant clean publish ide-quick -Ddist.bin.dir=$CEYLON_PATH -Dceylon.repo.dir=$CEYLON_REPO)
@@ -35,7 +35,7 @@ BUILD_DIR=`pwd`/build
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 mkdir $BUILD_DIR/ceylon
-cp -r ceylon-dist/dist/* $BUILD_DIR/ceylon
+cp -r ceylon/dist/dist/* $BUILD_DIR/ceylon
 mkdir $BUILD_DIR/eclipse-site
 cp -r ceylon-ide-eclipse/site/target/repository $BUILD_DIR/eclipse-site
 (cd build;tar -cvzf $BUILD_DIR/ceylon-dist.tgz ceylon)
